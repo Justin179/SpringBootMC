cc9fd8689f47a987836546d0c9a2eca9e38dc52d
Justin-Chen <justin.uob@gmail.com>
2021-11-28 00:01:52 +0800

    31%

--- src/main/java/com/example/demo/customer/CustomerController.java
+++ src/main/java/com/example/demo/customer/CustomerController.java
@@ -2,8 +2,7 @@ package com.example.demo.customer;
 
 import com.example.demo.DemoApplication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +20,22 @@ public class CustomerController {
     List<Customer> getCustomers(){
 
         return customerService.getCustomers();
+    }
+
+    @PostMapping
+    void createNewCustomer(@RequestBody Customer customer){
+        System.out.println("post request...");
+        System.out.println(customer);
+    }
+
+    @PutMapping
+    void updateCustomer(@RequestBody Customer customer){
+        System.out.println("update request...");
+        System.out.println(customer);
+    }
+
+    @DeleteMapping(path = "{customerId}")
+    void deleteCustomer(@PathVariable("customerId") Long id){
+        System.out.println("delete request for customer with id " + id);
     }
 }
